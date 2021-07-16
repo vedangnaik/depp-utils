842900dcebda97fce8a4478079b7646c7f89bf7f
Vedang Naik <vedang.a.naik@gmail.com>
2021-07-17 00:50:53 +0530

    Added 'main' check, removed testing stuff

--- scrape_course_and_program_ids.py
+++ scrape_course_and_program_ids.py
@@ -14,66 +14,69 @@ parser.add_argument('course_ids_file', type=argparse.FileType('w'), help="path t
 parser.add_argument('program_ids_file', type=argparse.FileType('w'), help="path to ASCII file to store scraped program IDs")
 args = parser.parse_args()
 
-# Set up the driver - driver path is passed in via sys.argv[1]
+# Chromedriver options
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--window-size=1920x1080')
-driver = webdriver.Chrome(executable_path=args.chromedriver_path, options=options)
-driver.get("https://artsci.calendar.utoronto.ca/listing-program-subject-areas")
 
-# Get the div containing all the tables.
-alphabetProgramTables = driver.find_element_by_css_selector("#block-fas-content > div > article > div > div").find_elements_by_tag_name('table')
+if __name__ == "__main__":
+    # Set up the driver
+    driver = webdriver.Chrome(executable_path=args.chromedriver_path, options=options)
+    driver.get("https://artsci.calendar.utoronto.ca/listing-program-subject-areas")
 
-# Get all links in all the tables first. The <a>s themselves will expire if we go to each link and then come back, but the links will remain constant.
-subjectAreaLinks = []
+    # Get the div containing all the tables.
+    alphabetProgramTables = driver.find_element_by_css_selector("#block-fas-content > div > article > div > div").find_elements_by_tag_name('table')
 
-for table in alphabetProgramTables:
-    # The first link in each alphabet's table is the one that scrolls the table to the top when that alphabet is clicked i.e. clicking 'B' at the top brings the #B <a> to the top.
-    for a in table.find_elements_by_tag_name('a')[1:]:
-        subjectAreaLinks.append(a.get_attribute('href'))
+    # Get all links in all the tables first. The <a>s themselves will expire if we go to each link and then come back, but the links will remain constant.
+    subjectAreaLinks = []
 
-# A few subject areas all link to the same page. Notably, these include those offered by colleges such as Trinity, University, etc. which all link to the respective college's page. Thus, we scrape some courses multiple times. This is to prevent such duplicates.
-coursesSeen = []
-cRegex = re.compile('^[A-Z]{3}[1-4][0-9]{2}[HY][01]$')
-# To weed out duplicate program IDs, just in case they come up somehow.
-programsSeen = []
-pRegex = re.compile('^AS(MAJ|SPE|MIN|FOC)[0-9]{4}.?$')
+    for table in alphabetProgramTables:
+        # The first link in each alphabet's table is the one that scrolls the table to the top when that alphabet is clicked i.e. clicking 'B' at the top brings the #B <a> to the top.
+        for a in table.find_elements_by_tag_name('a')[1:]:
+            subjectAreaLinks.append(a.get_attribute('href'))
 
-for link in subjectAreaLinks[:2]:
-    driver.get(link)
+    # A few subject areas all link to the same page. Notably, these include those offered by colleges such as Trinity, University, etc. which all link to the respective college's page. Thus, we scrape some courses multiple times. This is to prevent such duplicates.
+    coursesSeen = []
+    cRegex = re.compile('^[A-Z]{3}[1-4][0-9]{2}[HY][01]$')
+    # To weed out duplicate program IDs, just in case they come up somehow.
+    programsSeen = []
+    pRegex = re.compile('^AS(MAJ|SPE|MIN|FOC)[0-9]{4}.?$')
 
-    # This xpath always leads to the elements which contain the names of the courses.
-    coursesPs = driver.find_elements_by_xpath('//*[@id="block-fas-content"]/div/div/div/div[3]/div[2]/div[3]/div/p')
+    for link in subjectAreaLinks:
+        driver.get(link)
 
-    # This xpath always leads to the elements which contain the names of the programs.
-    programPs = driver.find_elements_by_xpath('//*[@id="block-fas-content"]/div/div/div/div[3]/div[1]/div[2]/div/p')
-    
-    for p in coursesPs:
-        # There's a space before the actual course ID, so we extract the first to ninth letters and output to stdout.
-        courseID = p.get_attribute('innerText')[1:9]
-        if cRegex.match(courseID) and courseID not in coursesSeen:
-            coursesSeen.append(courseID)
-            args.course_ids_file.write(courseID + "\n")
-        else:
-            print(f"Course {courseID} is a duplicate or failed the course ID regex.", file=sys.stderr)
+        # This xpath always leads to the elements which contain the names of the courses.
+        coursesPs = driver.find_elements_by_xpath('//*[@id="block-fas-content"]/div/div/div/div[3]/div[2]/div[3]/div/p')
 
-    for p in programPs:
-        # The program ID comes at the end of the full name, after the last '-' character. Some incomplete sentences do not have any ID at all, so these are ignored. We use a regex to see whether the program IDs match.
-        programID = p.get_attribute('innerText').split("-")[-1][1:]
-        if pRegex.match(programID) and programID not in programsSeen:
-            programsSeen.append(programID)
-            args.program_ids_file.write(programID + "\n")
-        else:
-            print(f"Program {programID} is a duplicate or failed the program ID regex.", file=sys.stderr)
+        # This xpath always leads to the elements which contain the names of the programs.
+        programPs = driver.find_elements_by_xpath('//*[@id="block-fas-content"]/div/div/div/div[3]/div[1]/div[2]/div/p')
+        
+        for p in coursesPs:
+            # There's a space before the actual course ID, so we extract the first to ninth letters and output to stdout.
+            courseID = p.get_attribute('innerText')[1:9]
+            if cRegex.match(courseID) and courseID not in coursesSeen:
+                coursesSeen.append(courseID)
+                args.course_ids_file.write(courseID + "\n")
+            else:
+                print(f"Course {courseID} is a duplicate or failed the course ID regex.", file=sys.stderr)
 
-# Print some diagnostics
-print("Finished.")
-print(f"Examined {len(subjectAreaLinks)} subject areas and scraped:")
-print(f"\t{len(coursesSeen)} courses")
-print(f"\t{len(programsSeen)} programs")
+        for p in programPs:
+            # The program ID comes at the end of the full name, after the last '-' character. Some incomplete sentences do not have any ID at all, so these are ignored. We use a regex to see whether the program IDs match.
+            programID = p.get_attribute('innerText').split("-")[-1][1:]
+            if pRegex.match(programID) and programID not in programsSeen:
+                programsSeen.append(programID)
+                args.program_ids_file.write(programID + "\n")
+            else:
+                print(f"Program {programID} is a duplicate or failed the program ID regex.", file=sys.stderr)
 
-# Close stuff
-args.course_ids_file.close()
-args.program_ids_file.close()
-driver.close()
-driver.quit()
+    # Print some diagnostics
+    print("Finished.")
+    print(f"Examined {len(subjectAreaLinks)} subject areas and scraped:")
+    print(f"\t{len(coursesSeen)} courses")
+    print(f"\t{len(programsSeen)} programs")
+
+    # Close stuff
+    args.course_ids_file.close()
+    args.program_ids_file.close()
+    driver.close()
+    driver.quit()
